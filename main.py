9bb28fc18c897e795424bb09d7ba907d0723e340
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2023-11-29 09:56:18 +0100

    Fix typo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import discord
 from logging import debug, info, warning, error, critical
 
 intents = discord.Intents.default()
-intents.message = True
+intents.messages = True
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 bot.remove_command('help')
